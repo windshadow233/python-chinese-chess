c1047be8ee769c7b9c9cba66ac74ccbc4232eb0c
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2022-01-23 14:51:43 +0800

    Update __init__.py

--- cchess/__init__.py
+++ cchess/__init__.py
@@ -2,7 +2,6 @@ from typing import Iterable, Union, SupportsInt, Iterator, Callable, List, Tuple
 import copy
 import dataclasses
 import enum
-import time
 
 Color = bool
 COLORS = [BLACK, RED] = [False, True]
